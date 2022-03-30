8e8d19b5235df706ab33d68e410d3fbd51e99b36
timwtuck <timwtucker@gmail.com>
2022-03-30 10:13:04 +0100

    Created a select by property method which I thought I'd need for the ticket, but turns out I don't. However it may prove to be useful

--- models/users.models.js
+++ models/users.models.js
@@ -1,5 +1,6 @@
 const db = require('../db/connection.js');
 const format = require('pg-format');
+const errors = require('../errors.js');
 
 exports.selectUsers = () => {
 
@@ -10,3 +11,35 @@ exports.selectUsers = () => {
             return res.rows;
         });
 }
+
+exports.selectUserByUsername = async (username) => {
+
+    return await this._selectByProperty('username', username);
+};
+
+
+
+
+/*******************************************************
+ * PRIVATE METHODS
+ *******************************************************/
+
+exports._selectByProperty = async (property, value) => {
+
+    const validProperties = ['username', 'name', 'avatar_url'];
+    if(!validProperties.includes(property))
+        return Promise.reject(errors.invalidDatabaseColummObj);
+
+    const query = `SELECT * FROM users
+                    WHERE %I = $1;`;
+
+    const sql = format(query, property);
+    const results = await db.query(sql, [value]);
+
+    if(results.length === 0)
+        return Promise.reject(errors.idNotFoundObj);
+    
+    return results.rows[0];
+}
+
+
